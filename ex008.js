a288e815b4da6ec984c727ee33c0188b994f06b4
Daniel VS <90420028+Daniel-VS2@users.noreply.github.com>
2022-02-22 18:46:29 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`a velocidade do seu automovel é ${vel} Km/h`)
+if (vel > 60 ) // Condição Simples visto que não possui a condição if 
+console.log (`Você ultrapassou o limite de velocidade.Multado!`)
+console.log(`dirija sempre usando o cinto de segurança`)
+
